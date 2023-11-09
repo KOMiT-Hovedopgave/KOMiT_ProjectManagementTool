f8bc3fbdb97d19204ccaaa0b38717a6555d70def
Ahmad Safwat Shanta <assh38002@edu.ucl.dk>
2023-11-09 12:57:10 +0100

    Added DbSet

--- KOMiT/KOMiT.DataAccess/Persistence/DatabaseContext.cs
+++ KOMiT/KOMiT.DataAccess/Persistence/DatabaseContext.cs
@@ -1,21 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KOMiT.Core.Model;
+using Microsoft.EntityFrameworkCore;
 
-namespace KOMiT.DataAccess.Persistence
+namespace KOMiT.DataAccess.Persistence;
+
+public class DatabaseContext : DbContext
 {
-    public class DatabaseContext : DbContext
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
     {
-
-            public DatabaseContext(DbContextOptions<DatabaseContext> options)
-                : base(options)
-            {
-            }
-
-
-
     }
+
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<SubProject> SubProjects { get; set; }
+    public DbSet<CurrentSubGoal> CurrentSubGoals { get; set; }
+    public DbSet<CurrentTask> CurrentTasks { get; set; }
+    public DbSet<StandardSubGoal> StandardSubGoals { get; set; }
+    public DbSet<StandardTask> StandardTasks { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<ProjectMember> ProjectMembers { get; set; }
+
 }
