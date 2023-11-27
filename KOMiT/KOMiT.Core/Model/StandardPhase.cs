bbfc5ef3246ba52e98b040895d048270e5512339
Ahmad Safwat Shanta <assh38002@edu.ucl.dk>
2023-11-27 09:17:07 +0100

    ConfirmationService added

--- KOMiT/KOMiT.Core/Model/StandardPhase.cs
+++ KOMiT/KOMiT.Core/Model/StandardPhase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace KOMiT.Core.Model;
 public class StandardPhase
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Description { get; set; }
 
     public ICollection<CurrentPhase> CurrentPhases { get; } = new List<CurrentPhase>();
